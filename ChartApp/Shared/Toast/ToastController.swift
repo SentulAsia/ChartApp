80287ed0a467d361b68d4d6395478b4a845bba0c
Zaid M. Said <zaid.shr@gmail.com>
2020-05-09 18:40:46 +0800

    added toast

---
+++ ChartApp/Shared/Toast/ToastController.swift
@@ -0,0 +1,30 @@
+//
+//  ToastController.swift
+//  ChartApp
+//
+//  Created by Zaid Said on 09/05/2020.
+//  Copyright © 2020 Zaid Said. All rights reserved.
+//
+
+import UIKit
+
+class ToastController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
